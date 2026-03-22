a7d5355bcbc1353c90a675b8872d496d0abc1463
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2026-03-22 18:10:42 -0400

    0.1.1 (The Dynamic Update)
    Dynamically removes all ways to create landfill and cliff explosives. Also updates to 2.X

--- nocliff/data-final-fixes.lua
+++ nocliff/data-final-fixes.lua
@@ -1,4 +1,12 @@
-data.raw.recipe['landfill'] = nil
-data.raw.recipe['cliff-explosives'] = nil
-data.raw.technology['landfill'] = nil
-data.raw.technology['cliff-explosives'] = nil
+for r, recipe in pairs(data.raw.recipe) do
+	if recipe.results ~= nil and next(recipe.results) ~= nil then
+		for res, result in pairs(recipe.results) do
+		    if result.name == 'landfill' or result.name == 'cliff-explosives' then
+		    	recipe.results[res].amount = 0
+		    end
+		end
+	end
+end
+
+data.raw.technology['landfill'].effects = nil
+data.raw.technology['cliff-explosives'].effects = nil
